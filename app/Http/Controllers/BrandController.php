fb3038165fb1121f47ecd693325c307c83655407
Phan Cong Tanh <ptoilatanh@gmail.com>
2023-09-11 08:55:02 +0700

    suaThuongHieu

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AddBrandRequest;
+use App\Http\Requests\UpdateBrandRequest;
 use App\Models\Brand;
 use Illuminate\Http\Request;
 
@@ -47,6 +48,36 @@ class BrandController extends Controller
         $data = Brand::where('ten_thuong_hieu', 'like', $giaTri)->get();
         return response()->json([
             'data' => $data,
+        ]);
+    }
+    public function edit(Request $request)
+    {
+        $brand = Brand::where('id', $request->id)->first();
+        if ($brand) {
+            return response()->json([
+                'status'    => true,
+                'data'   => $brand,
+            ]);
+        } else {
+            return response()->json([
+                'status'    => false,
+                'message'   => 'Dữ liệu không chính xác !',
+            ]);
+        }
+    }
+    public function update(UpdateBrandRequest $request)
+    {
+        $brand = Brand::where('id', $request->id)->first();
+        $brand->update([
+            'ten_thuong_hieu'    =>  $request->ten_thuong_hieu,
+            'slug_thuong_hieu'   =>  $request->slug_thuong_hieu,
+            'hinh_anh'          =>  $request->hinh_anh,
+            'tinh_trang'        =>  $request->tinh_trang,
+        ]);
+
+        return response()->json([
+            'status'    => true,
+            'message'   => 'Cập nhật mới thành công',
         ]);
     }
 }
